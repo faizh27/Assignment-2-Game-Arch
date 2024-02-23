d7ea12de8ca332df881d40a9e4fb8e00dcc40141
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2024-02-23 01:35:52 -0800

    removed all comments

--- Game-Architecture-Assignment-2/Assignment 2/Minimap.swift
+++ Game-Architecture-Assignment-2/Assignment 2/Minimap.swift
@@ -41,60 +41,6 @@ class Minimap: SKScene {
 
 
     func createGridCoordinates() {
-        //print("1: \(maze.GetCell(0, 0))")
-        // Calculate the cell size based on the screen size and number of rows/columns
-//        let cellWidth = minimapWidth / CGFloat(numberOfColumns)
-//        let cellHeight = minimapHeight / CGFloat(numberOfRows)
-//
-//        // Use the minimum of cellWidth and cellHeight to ensure cells fit within the screen
-//        cellSize = CGSize(width: min(cellWidth, cellHeight), height: min(cellWidth, cellHeight))
-//        
-//        let startX = -CGFloat(numberOfColumns) * cellSize.width / 2
-//        let startY = -CGFloat(numberOfRows) * cellSize.height / 2
-//        
-//        for row in 0..<numberOfRows {
-//            for col in 0..<numberOfColumns {
-////                let x = startX + CGFloat(col) * cellSize.width
-////                let y = startY + CGFloat(row) * cellSize.height
-////                let cellRect = CGRect(x: x, y: y, width: cellSize.width, height: cellSize.height)
-////                let cellNode = SKShapeNode(rect: cellRect, cornerRadius: 0)
-////                cellNode.strokeColor = .white
-////                cellNode.lineWidth = 2
-////                addChild(cellNode)
-//                let cell = maze.GetCell(Int32(row), Int32(col))
-//                let cellX = startX + CGFloat(col) * cellSize.width
-//                let cellY = -startY + CGFloat(row) * cellSize.height
-//                                
-//                // Draw walls based on cell properties
-//                if cell.southWallPresent {
-//                    let northWall = SKSpriteNode(color: wallColor, size: CGSize(width: cellSize.width, height: wallWidth))
-//                    northWall.position = CGPoint(x: cellX, y: cellY + cellSize.height / 2)
-//                    northWall.color = .blue
-//                    addChild(northWall)
-//                }
-//
-//                if cell.eastWallPresent {
-//                    let eastWall = SKSpriteNode(color: wallColor, size: CGSize(width: wallWidth, height: cellSize.height))
-//                    eastWall.position = CGPoint(x: cellX + cellSize.width / 2, y: cellY)
-//                    eastWall.color = .red
-//                    addChild(eastWall)
-//                }
-//
-//                if cell.northWallPresent {
-//                    let southWall = SKSpriteNode(color: wallColor, size: CGSize(width: cellSize.width, height: wallWidth))
-//                    southWall.position = CGPoint(x: cellX, y: cellY - cellSize.height / 2)
-//                    southWall.color = .green
-//                    addChild(southWall)
-//                }
-//
-//                if cell.westWallPresent {
-//                    let westWall = SKSpriteNode(color: wallColor, size: CGSize(width: wallWidth, height: cellSize.height))
-//                    westWall.position = CGPoint(x: cellX - cellSize.width / 2, y: cellY)
-//                    westWall.color = .black
-//                    addChild(westWall)
-//                }
-//            }
-//        }
         
         let startX = (UIScreen.main.bounds.size.width - minimapWidth) / 2
         let startY = (UIScreen.main.bounds.size.height - minimapHeight) / 2
